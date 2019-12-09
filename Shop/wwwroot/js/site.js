1e8a04809d4c8cd3e5e822874b3dc5c05f706a75
ayan <ayanpoddar9@gmail.com>
2019-12-09 21:55:58 +0530

    paging compleate

--- Shop/wwwroot/js/site.js
+++ Shop/wwwroot/js/site.js
@@ -1,5 +1,7 @@
 ﻿window.onload = function () {
     let x = document.getElementById("toggle");
+    let option = document.getElementsByName("categoryId")[0];
+    option.value = getCookie("category");
     x.onclick = function ()
     {
         let y = x.getElementsByTagName("div")[1];
@@ -12,4 +14,24 @@
             y.className = "dropdown-menu";
         }
     }
+    option.onchange = function () {
+        document.cookie = "category =" + this.value + ";";
+        document.getElementById('form').submit();
+    };
 };
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
