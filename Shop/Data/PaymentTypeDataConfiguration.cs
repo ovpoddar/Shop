973a6b499f2270ca4fb3f46932e76374e4d7f435
ayan poddar <ayanpoddar9@gmail.com>
2020-04-13 12:41:56 +0530

    fix the Conversation

---
+++ Shop/Data/PaymentTypeDataConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shop.Entities;
+using System;
+
+namespace Shop.Data
+{
+    public class PaymentTypeDataConfiguration : IEntityTypeConfiguration<Balance>
+    {
+        public void Configure(EntityTypeBuilder<Balance> builder)
+        {
+            builder.HasData(new Balance()
+            {
+                Id = 1,
+                Ammount = 500,
+                Date = DateTime.Now,
+                Incoming = 500,
+                PaymentTypeId = 2,
+                Outgoing = 0
+            });
+        }
+    }
+}
