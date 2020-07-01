a0d9bd42febc3ea7edd01fa8aba59821693b4cd2
ayan poddar <ayanpoddar9@gmail.com>
2020-07-01 13:45:41 +0530

    add data config for userrole

--- src/DataAccess/Configuration/IdentityUserRoleDataConfiguration.cs
+++ src/DataAccess/Configuration/IdentityUserRoleDataConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -8,14 +9,15 @@ using System.Text;
 
 namespace DataAccess.Configuration
 {
-    //public class IdentityUserRoleDataConfiguration : IEntityTypeConfiguration<IdentityUserRole>
-    //{
-    //    public void Configure(EntityTypeBuilder<IdentityUserRole> builder)
-    //    {
-    //        builder.HasData(new IdentityUserRole()
-    //        {
-
-    //        });
-    //    }
-    //}
+    public class IdentityUserRoleDataConfiguration : IEntityTypeConfiguration<IdentityUserRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole> builder)
+        {
+            builder.HasData(new IdentityUserRole()
+            {
+                RoleId = "1",
+                UserId = "1"
+            });
+        }
+    }
 }
