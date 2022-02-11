4312c99177f325c7b445af0b8a82cc040f419ab2
Ayan Poddar <ayanpoddar0@gmail.com>
2022-02-11 23:53:47 +0530

    update unkown

--- src/Shop/Controllers/ProductController.cs
+++ src/Shop/Controllers/ProductController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccess.Entities;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Managers;
 using Shop.Managers.Interfaces;
 using Shop.Utilities;
 using System;
+using System.Linq;
 
 namespace Shop.Controllers
 {
@@ -11,13 +14,17 @@ namespace Shop.Controllers
     public class ProductController : Controller
     {
         private readonly IProductManager _productManager;
-        public ProductController(IProductManager productManager) =>
+        private readonly IGenericRepository<Product> _repository;
+        public ProductController(IProductManager productManager, IGenericRepository<Product> repository)
+        {
             _productManager = productManager ?? throw new ArgumentNullException(nameof(ProductManager));
-
+            this._repository = repository;
+        }
 
         [HttpGet]
         public IActionResult Index()
         {
+            var c = _repository.GetAll().Sum(a =>a.Id);
             return View(_productManager.GetModel(1));
         }
 
